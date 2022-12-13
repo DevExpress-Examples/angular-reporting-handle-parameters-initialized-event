a5e0d7d45fad65582c702e30fb9d914537d90c75
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2022-12-13 19:17:57 +0400

    auto sync for version 2022.2

--- CS/ReportingWebApp/ClientApp/src/app/reportviewer/report-viewer.ts
+++ CS/ReportingWebApp/ClientApp/src/app/reportviewer/report-viewer.ts
@@ -6,7 +6,6 @@ import { DxReportViewerComponent } from 'devexpress-reporting-angular';
     encapsulation: ViewEncapsulation.None,
     templateUrl: './report-viewer.html',
     styleUrls: [
-        "../../../node_modules/jquery-ui/themes/base/all.css",
         "../../../node_modules/devextreme/dist/css/dx.common.css",
         "../../../node_modules/devextreme/dist/css/dx.light.css",
         "../../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.common.css",
@@ -16,28 +15,29 @@ import { DxReportViewerComponent } from 'devexpress-reporting-angular';
 })
 
 export class ReportViewerComponent {
-    @ViewChild(DxReportViewerComponent, { static: false }) viewer: DxReportViewerComponent;
+    @ViewChild(DxReportViewerComponent, { static: false })
+    viewer!: DxReportViewerComponent;
     reportUrl: string = "XtraReport1";
     invokeAction: string = '/DXXRDV';
 
-    OnParametersInitialized(event) {
+    OnParametersInitialized(event: any) {
         // Specify an invisible integer parameter's value on viewer initialization.
         var invisibleIntParamValue = 42;
         var intParam = event.args.ActualParametersInfo.filter(
-            x => x.parameterDescriptor.name == "intParam")[0];
+            (x: any) => x.parameterDescriptor.name == "intParam")[0];
         intParam.value(invisibleIntParamValue);
 
         // Specify a visible Boolean parameter's value on viewer initialization.
         var visibleBooleanParamValue = true;
         var booleanParam = event.args.ActualParametersInfo.filter(
-            x => x.parameterDescriptor.name == "booleanParam")[0];
+            (x: any) => x.parameterDescriptor.name == "booleanParam")[0];
         booleanParam.value(visibleBooleanParamValue);
 
         // Update a string parameter value when a user changes the Boolean parameter value.
         var strParam = event.args.ActualParametersInfo.filter(
-            x => x.parameterDescriptor.name == "strParam")[0];
+            (x: any) => x.parameterDescriptor.name == "strParam")[0];
 
-        booleanParam && booleanParam.value.subscribe(function (newVal) {
+        booleanParam && booleanParam.value.subscribe(function (newVal: any) {
             strParam.value(newVal.toString());
         });
 
