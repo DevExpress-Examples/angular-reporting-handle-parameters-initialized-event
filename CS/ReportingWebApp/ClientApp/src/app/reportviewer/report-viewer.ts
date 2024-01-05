b7bbf57785e0de70bdbff1ae24bf13ebb446f67e
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2024-01-05 18:47:59 +0400

    auto sync for version 2023.2

--- CS/ReportingWebApp/ClientApp/src/app/reportviewer/report-viewer.ts
+++ CS/ReportingWebApp/ClientApp/src/app/reportviewer/report-viewer.ts
@@ -25,20 +25,22 @@ export class ReportViewerComponent {
         var invisibleIntParamValue = 42;
         var intParam = event.args.ActualParametersInfo.filter(
             (x: any) => x.parameterDescriptor.name == "intParam")[0];
-        intParam.value(invisibleIntParamValue);
+        intParam.value = invisibleIntParamValue;
 
         // Specify a visible Boolean parameter's value on viewer initialization.
         var visibleBooleanParamValue = true;
         var booleanParam = event.args.ActualParametersInfo.filter(
             (x: any) => x.parameterDescriptor.name == "booleanParam")[0];
-        booleanParam.value(visibleBooleanParamValue);
+        booleanParam.value = visibleBooleanParamValue;
 
         // Update a string parameter value when a user changes the Boolean parameter value.
         var strParam = event.args.ActualParametersInfo.filter(
             (x: any) => x.parameterDescriptor.name == "strParam")[0];
 
-        booleanParam && booleanParam.value.subscribe(function (newVal: any) {
-            strParam.value(newVal.toString());
+        booleanParam && booleanParam.events.on('propertyChanged', (args: any) => {
+            if(args.propertyName === 'value') {
+                strParam.value = args.newVal.toString();
+            }
         });
 
         intParam & booleanParam & strParam && event.args.Submit();
